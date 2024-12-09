b9e301f3974c758116d8570d90515e0715151573
Simon <root@SimonPC>
2024-12-09 11:27:31 +1100

    992

---
+++ 992/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define space << " "
+#define int long long
+
+#define YES {cout << "YES" << endl;}
+#define NO {cout << "NO" << endl;}
+
+int32_t main () {
+    int cases;
+    cin >> cases;
+
+    while (cases--) {
+        int n,k;
+        cin >> n >> k;
+
+        vector<int> nums;
+        map<int,int> m;
+        for (int i = 1; i <= n; i++) {
+            int temp;
+            cin >> temp;
+            nums.push_back(temp);
+            m[temp] = i;
+        }
+        auto beg = nums.begin();
+
+        int res = 0;
+        while (beg != nums.end()) {
+            auto nex = nums.begin();
+            bool flag = true;
+            while (nex != nums.end()){
+                if ((nex != beg) and (((*nex - *beg) % k) == 0)) {
+                    flag = false;
+                }
+                nex = next(nex);
+            }
+
+            if (flag) {res = *beg; break;}
+            beg = next(beg);
+        }
+
+        if (res != 0) {
+            YES
+            cout << m[res] << endl;
+        } else {
+            NO;
+        }
+
+        nums.clear();
+        m.clear();
+    }
+
+    return 0;
+}
---
+++ 992/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define space << " "
+#define int long long
+
+#define YES {cout << "YES" << endl;}
+#define NO {cout << "NO" << endl;}
+
+
+int32_t main () {
+    int cases;
+    cin >> cases;
+
+    return 0;
+}
