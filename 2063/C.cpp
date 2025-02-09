929fae7f8c6997c0a0bb27e9731e858e3b52572e
Simon <root@SimonPC>
2025-02-10 00:42:28 +1100

    leet

--- 2063/C.cpp
+++ 2063/C.cpp
@@ -1,44 +1,68 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main () {
     int cases;
     cin >> cases;
     while(cases--) {
-        map<int,vector<int>> connections;
+        map<int,set<int>> connections;
         int nodes;
         cin >> nodes;
         for (int i = 0; i < nodes - 1; i++) {
             int a,b;
             cin >> a >> b;
-            connections[a].push_back(b);
-            connections[b].push_back(a);
+            connections[a].insert(b);
+            connections[b].insert(a);
         }
         int best = 1;
-        int count = 0;
+        vector<int> b_nodes;
         for (auto i : connections) {
-            if (i.second.size() > count) {
-                best = i.first;
-                count = 1;
-            } else if (i.second.size() == connections[best].size()) {
-                count++;
+            if (i.second.size() > best) {
+                best = i.second.size();
+                b_nodes.clear();
+                b_nodes.push_back(i.first);
+            } else if (i.second.size() == best) {
+                b_nodes.push_back(i.first);
             }
         }
         if (nodes == 2) cout << 0 << endl;
         else if (nodes == 3) cout << 1 << endl;
-        else if (count >= 3) cout << (connections[best].size() * 2) - 1;
         else {
-            int res = connections[best].size();
-            for (auto i : connections[best]) {
-                connections[i].pop_back();
+            int res = 0;
+            int cur_node = b_nodes[0];
+            for (auto i : connections[cur_node]) {
+                connections[i].erase(cur_node);
             }
-            connections[best].clear();
+            set<int> temp;
+            swap(connections[cur_node], temp);
             int second = 0;
             for (auto i : connections) {
                 second = max(second, int(i.second.size()));
             }
-            cout << res + second - 1 << endl;
+            swap(connections[cur_node], temp);
+            for (auto i : connections[cur_node]) {
+                connections[i].insert(cur_node);
+            }
+            res = max(int(connections[cur_node].size()) + second - 1, res);
+            if (b_nodes.size() >= 2) {
+                int cur_node = b_nodes[1];
+                for (auto i : connections[cur_node]) {
+                    connections[i].erase(cur_node);
+                }
+                set<int> temp;
+                swap(connections[cur_node], temp);
+                int second = 0;
+                for (auto i : connections) {
+                    second = max(second, int(i.second.size()));
+                }
+                swap(connections[cur_node], temp);
+                for (auto i : connections[cur_node]) {
+                    connections[i].insert(cur_node);
+                }
+                res = max(int(connections[cur_node].size()) + second - 1, res);
+            }
+
+            cout << res << endl;
         }
     }
     return 0;
